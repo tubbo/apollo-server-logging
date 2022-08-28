730a99e8447619a62e8a68cc49adf8ff2e18eb2f
Tom Scott <tscott@nerdstgamers.com>
2022-08-28 16:03:44 -0400

    test: add tests for cleaning variables

---
+++ clean.test.ts
@@ -0,0 +1,8 @@
+import { clean } from './clean'
+
+test('clean variable names', () => {
+  const variables = clean(['password'], { name: 'foo', password: 'bar' })
+
+  expect(variables.name).toEqual('foo')
+  expect(variables.password).toEqual('[REDACTED]')
+})
--- clean.ts
+++ clean.ts
@@ -1,5 +1,3 @@
-export const DIRTY_VARIABLE_NAMES: string[] = ['password', 'token']
-
 export type Variables = {
   [name: string]: unknown
 }
