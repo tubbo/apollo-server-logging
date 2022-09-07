720721c5c81ce4129577f1771a0549fed0306eea
Tom Scott <tscott@nerdstgamers.com>
2022-09-07 02:01:51 -0400

    docs: add docs for clean fn

--- clean.ts
+++ clean.ts
@@ -1,9 +1,19 @@
+/**
+ * Variables to pass into `clean()`.
+ */
 export type Variables = {
   [name: string]: unknown
 }
 
 /**
- * Clean secret variables for the log.
+ * Clean secret variables for the log. Replaces the value of the keys
+ * in the first argument with `'[REDACTED]'` in the second argument.
+ *
+ * @example
+ *  import { clean } from 'apollo-server-logging/clean'
+ *
+ *  clean(['foo'], { foo: 'bar', baz: 'bat' })
+ *  // => { foo: '[REDACTED], baz: 'bat' }
  */
 export function clean(names: string[], variables?: Variables): Variables {
   const cleanedVariables: Variables = { ...variables }
